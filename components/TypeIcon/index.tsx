4a26ccc87ffac1ac6f857b581541f86608f317f1
emquiroga <emiliano.quiroga093@gmail.com>
2022-03-06 04:00:10 -0300

    Added color function

--- components/TypeIcon/index.tsx
+++ components/TypeIcon/index.tsx
@@ -1,7 +1,8 @@
+import { typeToColor } from "../../helpers/typeToColor";
 import styles from "./index.module.css";
 
 export const TypeIcon = () => {
     return (
-        <span className={styles["type-shape"]} />
+        <span className={styles["type-shape"]} style={{ backgroundColor: typeToColor("fairy") }} />
     )
 }
