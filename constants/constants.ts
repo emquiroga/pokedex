5d8069a5154fa4adb678eae903020a526823a738
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-10 00:00:42 -0300

    added iterable data

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,1 +1,12 @@
-export const pokemonGenerations = [1, 2, 3, 4, 5, 6, 7, 8];
+export const NAVIGATION_ITEMS = [
+    {
+        name: 'Home',
+        url: '/',
+    },
+    {
+        name: 'About',
+        url: '/about',
+    },
+]
+
+export const POKEMON_GENERATIONS = [1, 2, 3, 4, 5, 6, 7, 8];
