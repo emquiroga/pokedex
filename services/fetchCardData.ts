0e24ff5803cba49a8394838297f3dbd5d25a854f
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-12 21:11:26 -0300

    refactor services

--- services/fetchCardData.ts
+++ services/fetchCardData.ts
@@ -1,32 +1,42 @@
 export type CardData = {
-    id: number;
-    name: string;
-    front_default: string;
-    types: [{
-        slot: number
-        type: {
-            name: string
-            url: string
-        }
-    }]
+  id: number;
+  name: string;
+  front_default: string;
+  types: [
+    {
+      slot: number;
+      type: {
+        name: string;
+        url: string;
+      };
+    },
+  ];
 };
 
-interface PokemonData {
-    id: number;
-    name: string;
-    sprites: {
-        front_default: string;
-    }
-    types: [{
-        slot: number
-        type: {
-            name: string
-            url: string
-        }
-    }]
+interface CardDataProps {
+  id: number;
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  types: [
+    {
+      slot: number;
+      type: {
+        name: string;
+        url: string;
+      };
+    },
+  ];
 }
 
-export const fetchCardData = (data: PokemonData) => {
-    const { id, name, sprites: { front_default }, types } = data;
-    return { id, name, front_default, types };
-}
+export const fetchCardData = (data: CardDataProps) => {
+  const {
+    id,
+    name,
+    sprites: { front_default },
+    types,
+  } = data;
+
+  return { id, name, front_default, types };
+};
--- services/fetchInfoData.ts
+++ services/fetchInfoData.ts
@@ -1,32 +1,78 @@
 export type InfoData = {
-    id: number;
-    name: string;
-    sprites: {
-        front_default: string;
-    }
-    height: number;
-    weight: number;
-    abilities: [{
-        ability: {
-            name: string;
-        }
-    }];
-    stats: [{
-        base_stat: number;
-        stat: {
-            name: string;
-        }
-    }];
-    base_experience: number;
-    types: [{
-        type: {
-            name: string
-            url: string
-        }
-    }]
+  name: string;
+  front_default: string;
+  height: number;
+  weight: number;
+  abilities: [
+    {
+      ability: {
+        name: string;
+      };
+    },
+  ];
+  stats: [
+    {
+      base_stat: number;
+      stat: {
+        name: string;
+      };
+    },
+  ];
+  base_experience: number;
+  types: [
+    {
+      type: {
+        name: string;
+        url: string;
+      };
+    },
+  ];
+};
+
+interface InfoDataProps {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  height: number;
+  weight: number;
+  abilities: [
+    {
+      ability: {
+        name: string;
+      };
+    },
+  ];
+  stats: [
+    {
+      base_stat: number;
+      stat: {
+        name: string;
+      };
+    },
+  ];
+  base_experience: number;
+  types: [
+    {
+      type: {
+        name: string;
+        url: string;
+      };
+    },
+  ];
 }
 
-export const fetchInfoData = (data: InfoData) => {
-    const { id, name, sprites: { front_default }, height, weight, abilities, stats, base_experience, types } = data;
-    return { id, name, front_default, height, weight, abilities, stats, base_experience, types };
-}
+export const fetchInfoData = (data: InfoDataProps) => {
+  const {
+    name,
+    sprites: { front_default },
+    height,
+    weight,
+    abilities,
+    stats,
+    base_experience,
+    types,
+  } = data;
+
+  return { name, front_default, height, weight, abilities, stats, base_experience, types };
+};
