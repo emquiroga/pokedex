7d11c2a64905250a1f12d7efbe38a1f47c8395b2
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-12 22:27:50 -0300

    eslinted

--- helpers/generationRanges.ts
+++ helpers/generationRanges.ts
@@ -1,52 +1,51 @@
 export const generationRanges = (generation: string) => {
-    switch (generation) {
-        //Start is the number of the first pokemon by generations less one
-        //End is the total of pokemons by each geneation less one
-        case "1":
-            return {
-                start: 0,
-                end: 151
-            };
-        case "2":
-            return {
-                start: 151,
-                end: 99
-            };
-        case "3":
-            return {
-                start: 251,
-                end: 134
-            };
-        case "4":
-            return {
-                start: 386,
-                end: 106
-            };
-        case "5":
-            return {
-                start: 493,
-                end: 155
-            };
-        case "6":
-            return {
-                start: 694,
-                end: 71
-            };
-        case "7":
-            return {
-                start: 721,
-                end: 87
-            };
-        case "8":
-            return {
-                start: 809,
-                end: 95
-            };
-        default:
-            return {
-                start: 0,
-                end: 0
-            }
-
-    }
-}
+  switch (generation) {
+    //Start is the number of the first pokemon by generations less one
+    //End is the total of pokemons by each geneation less one
+    case "1":
+      return {
+        start: 0,
+        end: 151,
+      };
+    case "2":
+      return {
+        start: 151,
+        end: 99,
+      };
+    case "3":
+      return {
+        start: 251,
+        end: 134,
+      };
+    case "4":
+      return {
+        start: 386,
+        end: 106,
+      };
+    case "5":
+      return {
+        start: 493,
+        end: 155,
+      };
+    case "6":
+      return {
+        start: 694,
+        end: 71,
+      };
+    case "7":
+      return {
+        start: 721,
+        end: 87,
+      };
+    case "8":
+      return {
+        start: 809,
+        end: 95,
+      };
+    default:
+      return {
+        start: 0,
+        end: 0,
+      };
+  }
+};
