5153d494e067be96b42f9380620045b133624f5e
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-12 21:14:23 -0300

    eslinted

--- components/PokedexLayout/index.tsx
+++ components/PokedexLayout/index.tsx
@@ -1,16 +1,17 @@
 import { ReactChildren } from "react";
-import styles from "./index.module.css"
+
+import styles from "./index.module.css";
 
 interface PokedexLayoutProps {
-    children: JSX.Element | ReactChildren;
+  children: JSX.Element | ReactChildren;
 }
 
 export const PokedexLayout = ({ children }: PokedexLayoutProps) => {
-    return (
-        <div className={styles["pokedex_outer_shape"]}>
-            <span className={styles["top-shape"]} />
-            {children}
-            <span className={styles["bottom-shape"]} />
-        </div>
-    )
-}
+  return (
+    <div className={styles["pokedex_outer_shape"]}>
+      <span className={styles["top-shape"]} />
+      {children}
+      <span className={styles["bottom-shape"]} />
+    </div>
+  );
+};
--- components/Table/index.tsx
+++ components/Table/index.tsx
@@ -1,40 +1,42 @@
 import Link from "next/link";
 interface TableProps {
-    titles: string[];
-    data: [
-        {
-            name: string;
-            url: string;
-        }
-    ]
+  titles: string[];
+  data: [
+    {
+      name: string;
+      url: string;
+    },
+  ];
 }
 
 const getPokeNumber = (url: string) => {
-    return url.split("/")[6];
-}
+  return url.split("/")[6];
+};
 
-export const Table = ({titles, data}: TableProps) => {
-    return (
-        <table className="table-auto">
-            <thead>
-                    <tr>
-                        {                    
-                            titles.map((title, index) => (<th key={index} className="border px-4 py-2">{title}</th>))
-                        }   
-                    </tr>
-            </thead>
-            <tbody>
-                        {
-                            data.map((item, index) => {
-                                return(
-                                    <tr key={index}>
-                                        <td className="border px-4 py-2">{item.name}</td>
-                                        <td className="border px-4 py-2"><Link href={`/pokemon/${getPokeNumber(item.url)}`}>See more</Link></td>
-                                    </tr>
-                                )
-                            })
-                        }
-            </tbody>
-        </table>
-    )
-}
+export const Table = ({ titles, data }: TableProps) => {
+  return (
+    <table className="table-auto">
+      <thead>
+        <tr>
+          {titles.map((title, index) => (
+            <th key={index} className="border px-4 py-2">
+              {title}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => {
+          return (
+            <tr key={index}>
+              <td className="border px-4 py-2">{item.name}</td>
+              <td className="border px-4 py-2">
+                <Link href={`/pokemon/${getPokeNumber(item.url)}`}>See more</Link>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
