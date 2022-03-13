41170cd71bcc5d7f9baa29fc87360033aaf02fc9
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-12 21:10:55 -0300

    eslinted

--- components/TypeIcon/index.tsx
+++ components/TypeIcon/index.tsx
@@ -1,8 +1,7 @@
 import { typeToColor } from "../../helpers/typeToColor";
+
 import styles from "./index.module.css";
 
 export const TypeIcon = ({ type }: { type: string }) => {
-    return (
-        <span className={styles["type-shape"]} style={{ backgroundColor: typeToColor(type) }} />
-    )
-}
+  return <span className={styles["type-shape"]} style={{ backgroundColor: typeToColor(type) }} />;
+};
