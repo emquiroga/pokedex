0f74b45fec61b24624b281762394a2568c4411b8
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-15 21:45:05 -0300

    eslinted

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,12 +1,12 @@
 export const NAVIGATION_ITEMS = [
-    {
-        name: 'Home',
-        url: '/',
-    },
-    {
-        name: 'About',
-        url: '/about',
-    },
-]
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "About",
+    url: "/about",
+  },
+];
 
 export const POKEMON_GENERATIONS = [1, 2, 3, 4, 5, 6, 7, 8];
