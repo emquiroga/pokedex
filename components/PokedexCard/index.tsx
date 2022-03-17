224ed129a64e4e7778fff230f353d2a2df2ba43a
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-17 17:34:43 -0300

    Fixed types container position

--- components/PokedexCard/index.tsx
+++ components/PokedexCard/index.tsx
@@ -12,13 +12,17 @@ export const PokedexCard = ({ name, image, id, types }: CardTypes) => {
       <h1 className={styles["card-title"]}>{name}</h1>
       <Image alt={name} height="200" src={image} width="200" />
       {types && (
-        <div className={styles["types-wrapper"]}>
+        <div className={styles["types-container"]}>
           {types.map(({ type: { name } }) => {
-            return <TypeIcon key={name} type={name} />;
+            return (
+              <span key={name} className={styles["type-wrapper"]}>
+                <TypeIcon type={name} />
+              </span>
+            );
           })}
         </div>
       )}
-      <p className={styles["card-id"]}>{id}</p>
+      <p className={styles["card-id"]}>#{id}</p>
       <Link href={`/pokemon/${id}`} passHref={true}>
         <p className={styles["card-link"]}>See more</p>
       </Link>
