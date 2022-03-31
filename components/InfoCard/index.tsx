54cbc9852f88ac74dd4039b75e82430d17acd2d6
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-31 12:01:42 -0300

    Added next and prev pokemon in pokemon page

--- components/InfoCard/index.tsx
+++ components/InfoCard/index.tsx
@@ -1,9 +1,42 @@
 import Image from "next/image";
 
-import { InfoData } from "../../services/fetchInfoData";
 import { TypeIcon } from "../TypeIcon";
 
 import styles from "./index.module.css";
+
+interface InfoCardProps {
+  name: string;
+  id: number;
+  front_default: string;
+  height: number;
+  weight: number;
+  abilities: [
+    {
+      ability: {
+        name: string;
+      };
+    }
+  ];
+  stats: [
+    {
+      base_stat: number;
+      stat: {
+        name: string;
+      };
+    }
+  ];
+  base_experience: number;
+  types: [
+    {
+      type: {
+        name: string;
+        url: string;
+      };
+    }
+  ];
+  handleNext: () => void;
+  handlePrev: () => void;
+}
 
 export const InfoCard = ({
   name,
@@ -14,8 +47,10 @@ export const InfoCard = ({
   abilities,
   stats,
   base_experience,
-  types
-}: InfoData) => {
+  types,
+  handleNext,
+  handlePrev
+}: InfoCardProps) => {
   return (
     <div className={`bg-darkRed rounded-xl ${styles["info-card-container"]}`}>
       <div className={`w-full my-0 ${styles["header-title"]}`}>
@@ -84,6 +119,46 @@ export const InfoCard = ({
             </ul>
           </div>
         </div>
+      </div>
+      <div
+        className={`w-full max-w-sm my-2 flex flex-row justify-around content-center ${styles["navigation-container"]}`}
+      >
+        <button
+          className={`flex flex-col justify-center content-center h-14 bg-yellow p-4 rounded-md text-lightRed font-bold text-large ${styles["navigation-btn"]}`}
+          type="button"
+          onClick={handlePrev}
+        >
+          <svg
+            className="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clipRule="evenodd"
+              d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+              fillRule="evenodd"
+            />
+          </svg>
+        </button>
+        <button
+          className={`flex flex-col justify-center content-center h-14 bg-yellow p-4 rounded-md text-lightRed font-bold text-large ${styles["navigation-btn"]}`}
+          type="button"
+          onClick={handleNext}
+        >
+          <svg
+            className="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clipRule="evenodd"
+              d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
+              fillRule="evenodd"
+            />
+          </svg>
+        </button>
       </div>
       <div className={styles["info-card__stats-container"]}>
         <h3
